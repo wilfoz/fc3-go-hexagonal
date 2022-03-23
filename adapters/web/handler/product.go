16d683510c6e693b03efd4022afefc8901c27f08
wilfoz <wil_foz@yahoo.com.br>
2022-03-22 21:28:27 -0300

    create handler product

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,32 @@
+package handler
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/codegangsta/negroni"
+	"github.com/fc3/go-hexagonal/application"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/product/{id}/enable", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
---
+++ adapters/web/server/server.go
@@ -0,0 +1,41 @@
+package server 
+
+import (
+	"os"
+	"log"
+	"time"
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/codegangsta/negroni"
+	"github.com/fc3/go-hexagonal/application"
+	"github.com/fc3/go-hexagonal/adapters/web/handler"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+
+	r := mux.NewRouter()
+	n := negroni.New(negroni.NewLogger(),)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr: ":8080",
+		Handler: http.DefaultServeMux,
+		ErrorLog: log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
